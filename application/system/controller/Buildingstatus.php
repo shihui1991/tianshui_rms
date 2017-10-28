cb69d591e5370d2060b523a9712a1f5fa4a4f94f
bulian <452853270@qq.com>
2017-10-28 15:18:14 +0800

    建筑状况

---
+++ application/system/controller/Buildingstatus.php
@@ -0,0 +1,232 @@
+<?php
+/* |------------------------------------------------------
+ * | 建筑状况
+ * |------------------------------------------------------
+ * | 初始化操作
+ * | 列表
+ * | 添加
+ * | 详情
+ * | 修改
+ * | 状态
+ * | 删除
+ * | 恢复
+ * | 销毁
+ * */
+namespace app\system\controller;
+
+use app\system\model\Buildingstatuss;
+use think\Db;
+
+class Buildingstatus extends Auth
+{
+    /* ========== 初始化 ========== */
+    public function _initialize()
+    {
+        parent::_initialize();
+
+    }
+
+    /* ========== 列表 ========== */
+    public function index()
+    {
+        /* ********** 查询条件 ********** */
+        $datas=[];
+        $where=[];
+        $field=['id','name','infos','status','deleted_at'];
+        /* ++++++++++ 名称 ++++++++++ */
+        $name=trim(input('name'));
+        if($name){
+            $where['name']=['like','%'.$name.'%'];
+            $datas['name']=$name;
+        }
+        /* ++++++++++ 状态 ++++++++++ */
+        $status=input('status');
+        if(is_numeric($status) && in_array($status,[0,1])){
+            $where['status']=$status;
+            $datas['status']=$status;
+        }
+        /* ++++++++++ 排序 ++++++++++ */
+        $ordername=input('ordername');
+        $ordername=$ordername?$ordername:'id';
+        $datas['ordername']=$ordername;
+        $orderby=input('orderby');
+        $orderby=$orderby?$orderby:'asc';
+        $datas['orderby']=$orderby;
+        /* ++++++++++ 每页条数 ++++++++++ */
+        $nums=[config('paginate.list_rows'),30,50,100,200,500];
+        sort($nums);
+        $datas['nums']=$nums;
+        $display_num=input('display_num');
+        $display_num=$display_num?$display_num:config('paginate.list_rows');
+        $datas['display_num']=$display_num;
+        /* ++++++++++ 是否删除 ++++++++++ */
+        $deleted=input('deleted');
+        $buildingstatus_model=new Buildingstatuss();
+        if(is_numeric($deleted) && in_array($deleted,[0,1])){
+            $datas['deleted']=$deleted;
+            if($deleted==1){
+                $buildingstatus_model=$buildingstatus_model->onlyTrashed();
+            }
+        }else{
+            $buildingstatus_model=$buildingstatus_model->withTrashed();
+        }
+        $buildingstatuss=$buildingstatus_model->where($where)->field($field)->order([$ordername=>$orderby])->paginate($display_num);
+
+        $datas['buildingstatuss']=$buildingstatuss;
+
+        $this->assign($datas);
+
+        return view();
+    }
+
+    /* ========== 添加 ========== */
+    public function add($id=0){
+        $model=new Buildingstatuss();
+        if(request()->isPost()){
+            $rules=[
+                'name'=>'require|unique:building_status',
+            ];
+            $msg=[
+                'name.require'=>'名称不能为空',
+                'name.unique'=>'名称已存在',
+            ];
+
+            $result=$this->validate(input(),$rules,$msg);
+            if(true !== $result){
+                return $this->error($result);
+            }
+
+            $other_datas=$model->other_data(input());
+            $datas=array_merge(input(),$other_datas);
+            $model->save($datas);
+            if($model !== false){
+                return $this->success('保存成功','');
+            }else{
+                return $this->error('保存失败');
+            }
+        }else{
+            return view('modify',[
+                'model'=>$model,
+            ]);
+        }
+    }
+
+    /* ========== 详情 ========== */
+    public function detail($id=null){
+        if(!$id){
+            return $this->error('至少选择一项');
+        }
+        $infos=Buildingstatuss::withTrashed()->find($id);
+        if(!$infos){
+            return $this->error('选择项目不存在');
+        }
+
+        $model=new Buildingstatuss();
+
+        return view('modify',[
+            'model'=>$model,
+            'infos'=>$infos,
+        ]);
+    }
+
+    /* ========== 修改 ========== */
+    public function edit(){
+        $id=input('id');
+        if(!$id){
+            return $this->error('错误操作');
+        }
+        $datas=input();
+        $rules=[
+            'name'=>'require|unique:building_status,name,'.$id.',id',
+        ];
+        $msg=[
+            'name.require'=>'名称不能为空',
+            'name.unique'=>'名称已存在',
+        ];
+
+        $result=$this->validate($datas,$rules,$msg);
+        if(true !== $result){
+            return $this->error($result);
+        }
+
+        $buildingstatus_model=new Buildingstatuss();
+        $other_datas=$buildingstatus_model->other_data(input());
+        $datas=array_merge(input(),$other_datas);
+        $buildingstatus_model->isUpdate(true)->save($datas);
+        if($buildingstatus_model !== false){
+            return $this->success('修改成功','');
+        }else{
+            return $this->error('修改失败');
+        }
+    }
+
+    /* ========== 状态 ========== */
+    public function status(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+        $status=input('status');
+
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        if(!in_array($status,[0,1])){
+            return $this->error('错误操作');
+        }
+        $model=new Buildingstatuss();
+        $res=$model->save(['status'=>$status],['id'=>['in',$ids]]);
+        if($res){
+            return $this->success('修改成功','');
+        }else{
+            return $this->error('修改失败');
+        }
+    }
+
+    /* ========== 删除 ========== */
+    public function delete(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        $res=Buildingstatuss::destroy($ids);
+        if($res){
+            return $this->success('删除成功','');
+        }else{
+            return $this->error('删除失败');
+        }
+    }
+
+    /* ========== 恢复 ========== */
+    public function restore(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+
+        $res=Db::table('building_status')->whereIn('id',$ids)->update(['deleted_at'=>null,'updated_at'=>time()]);
+        if($res){
+            return $this->success('恢复成功','');
+        }else{
+            return $this->error('恢复失败');
+        }
+    }
+
+    /* ========== 销毁 ========== */
+    public function destroy(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        $res=Buildingstatuss::onlyTrashed()->whereIn('id',$ids)->delete(true);
+        if($res){
+            return $this->success('销毁成功','');
+        }else{
+            return $this->error('销毁失败，请先删除！');
+        }
+    }
+}
