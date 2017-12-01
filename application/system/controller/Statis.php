69c163ef98a131d20a5b083beb54ff5aed79ca98
seek-concert <1105420247@qq.com>
2017-12-01 11:33:58 +0800

    张_资金统计图标上传

---
+++ application/system/controller/Statis.php
@@ -0,0 +1,18 @@
+<?php
+/* |------------------------------------------------------
+ * | 统计与图表
+ * |------------------------------------------------------
+ * | 资金
+ * */
+namespace app\system\controller;
+
+
+class Statis extends Base
+{
+    /* ========== 初始化 ========== */
+    public function _initialize()
+    {
+        parent::_initialize();
+
+    }
+}
