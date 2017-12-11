e66ae8fe4050e4f8c5080dd61b3e0f08286f1dbe
bulian <452853270@qq.com>
2017-12-11 17:56:57 +0800

    被征户 入户摸底基本信息

---
+++ application/holder/controller/Collectionholder.php
@@ -0,0 +1,24 @@
+<?php
+/* |------------------------------------------------------
+ * | 入户摸底 产权人及家庭成员
+ * |------------------------------------------------------
+ * */
+namespace app\holder\controller;
+
+class Collectionholder extends Base
+{
+    /* ========== 初始化 ========== */
+    public function _initialize()
+    {
+        parent::_initialize();
+
+
+    }
+
+    /* ========== 列表 ========== */
+    public function index()
+    {
+
+    }
+
+}
